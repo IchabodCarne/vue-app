59aab3e8c8a36aadcda2a4d5cfe4f391498698c1
Ichabod-Carne <18829025242@163.com>
2023-03-14 20:14:08 +0800

    router.js逻辑修改

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,6 +1,8 @@
 import type { RouteRecordRaw } from 'vue-router'
 // 布局组件
 import Layout from '@/layout/home.vue'
+// 登录页面
+import Login from '@/views/login/index.vue'
 // 路由配置
 const routes: RouteRecordRaw[] = []
 // 所有页面组件
@@ -13,19 +15,23 @@ const layoutRouter: RouteRecordRaw = {
   component: Layout,
   children: []
 }
-routes.push(layoutRouter)
+// 登录页面
+const loginRouter: RouteRecordRaw = {
+  path: '/login',
+  name: 'login',
+  component: Login
+}
+routes.push(layoutRouter, loginRouter)
 
 // 页面组件路由定义
 Object.entries(views).forEach(([file, component]) => {
   // @ts-ignore
-  const path = file.split('/').pop().slice(0, -4)
-  if (path.toLowerCase() === 'login') {
-    routes.push({
-      path: '/login',
-      name: 'login',
-      component
-    })
-  } else {
+  let path = file.split('/').pop().slice(0, -4)
+  if (path === 'index') {
+    // 若文件夹下只有一个vue页面，以文件夹名称作为路由path
+    path = file.split('/').at(-2) ?? ''
+  }
+  if (path && path.toLowerCase() !== 'login') {
     layoutRouter.children.push({
       path: `/${path.toLowerCase()}`,
       component: component
